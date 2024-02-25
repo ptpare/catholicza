09326e2f6640c417ace4e060ee1855a22dcc1b2e
Phil111 <15924816+ptpare@users.noreply.github.com>
2024-02-26 01:59:41 +0200

    Update server.js
    app.listen(process.env.PORT || 3000, () => {
      console.log('Server is running on port', process.env.PORT || 3000);

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/proxy', (req, res) => {
   request(url).pipe(res);
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port', process.env.PORT || 3000);
 });
