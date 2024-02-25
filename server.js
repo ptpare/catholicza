429ad85f1a862b9789ad68912060b83b33b853c5
Phil111 <15924816+ptpare@users.noreply.github.com>
2024-02-26 01:36:33 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const request = require('request');
+
+const app = express();
+
+// Use cors middleware and allow all origins
+app.use(cors());
+
+app.get('/proxy', (req, res) => {
+  // Take the url from the query params
+  const url = req.query.url;
+
+  // Send a request to the original url
+  request(url).pipe(res);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
